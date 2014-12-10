63a29f976d490f89da5017087d376e49067b7f7e
sosubramanian <sosubramanian@ebay.com>
2014-12-10 10:10:12 -0500

    if pr is present send email

--- app/mailers/pull_request_mailer.rb
+++ app/mailers/pull_request_mailer.rb
@@ -11,10 +11,12 @@ class PullRequestMailer < ActionMailer::Base
   def review_pull_request_email(hours, pull_requests, users, to, teamName)
     puts "Inside Mailer"
     time_now = Time.now.in_time_zone("Eastern Time (US & Canada)")
-    if hours.include?(time_now.hour) && time_now.min == 0
-      if !(time_now.saturday? || time_now.sunday?)
-        send_email(pull_requests, users, to, teamName).deliver
-        puts "Email sent at : " + time_now.to_s
+    if pull_requests.length > 0
+      if hours.include?(time_now.hour) && time_now.min == 0
+        if !(time_now.saturday? || time_now.sunday?)
+          send_email(pull_requests, users, to, teamName).deliver
+          puts "Email sent at : " + time_now.to_s
+        end
       end
     end
   end
